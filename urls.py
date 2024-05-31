37265b414be0a133fbfa4f44ae9252f753bc7ba4
AmeerRoshan25 <114046618+AmeerRoshan25@users.noreply.github.com>
2024-05-31 07:31:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,31 @@
-"""climate URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('cproject.urls')),
+from django.urls import path
+from . import views
+from django.contrib.auth import views as ad
+urlpatterns=[
+    path('',views.start,name="ho"),
+    path('sign_up/',views.signup,name="log"),
+    path('otp_verfication/',views.otpcheck,name="otp"),
+    path('create_password/',views.createpass,name="sep"),
+    path('login/',views.loginuser,name="lou"),
+    path('profile/',views.profile,name="pro"),
+    path('news/',views.news,name="new"),
+    path('Alters/',views.alters,name="alt"),
+    path('login_incorrect/',views.loginc,name="loginc"),
+    path('temp/',views.temp,name="temp"),
+    path('notf/',views.notfound,name="notf"),
+    path('Forgotpassword/',views.forgotpassword,name="forp"),
+    path('otpcheck/',views.otppassword,name="forotp"),
+    path('changepassword/',views.changepassword,name="chgp"),
+    path('logout/',views.logou,name="loout"),
+    #policy makers code starts from here
+    #path('polog/',views.polog,name="polog"),
+    path('plgn/',ad.LoginView.as_view(template_name="html/pologin.html"),name="plog"),
+    path('plout/',ad.LogoutView.as_view(template_name="html/start.html"),name="lgt"),
+    #username=adminAAYH
+    #password=AAYH123
+    path('pancl/',views.paclimate,name="panc"),
+    path('grp/',views.graph,name="grp"),
+    path('indiagraphs/',views.indgrp,name="indg"),
+    path('predict/',views.predict,name="pred"),
+    path('comm/',views.commits,name="com"),
 ]
